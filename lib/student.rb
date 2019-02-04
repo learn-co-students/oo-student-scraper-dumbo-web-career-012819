80882588952180633efd3e52c230c3e5af9d709d
mazenswar <mazenswar@protonmail.com>
2019-02-04 12:54:12 -0500

    student scraper

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,24 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url, :student_hash
 
   @@all = []
 
   def initialize(student_hash)
-    
+    # attributes.each {|key, value| self.send(("#{key}="), value)}
+    student_hash.each { |k,v| self.send("#{k}=", v)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each{ |student| Student.new(student) }
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|k,v| self.send("#{k}=", v)}
   end
 
   def self.all
-    
+    @@all
   end
 end
-
